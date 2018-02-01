a05715b529da2c660d98e1abb482d8d593e3fbec
Chanpreet08 <chanpreet.chhabra@gmail.com>
2018-02-01 12:43:30 +0530

    Token based authentication

--- login-Flask/venv/login.py
+++ login-Flask/venv/login.py
@@ -1,13 +1,36 @@
-from flask import Flask, render_template , url_for,request, session , redirect
+from flask import Flask, render_template , url_for,request, session , redirect,jsonify
 from flask_pymongo import PyMongo
 import bcrypt
+import jwt 
+from bson import json_util,ObjectId
+import config
+import json
+import datetime
 
 app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'mongoExample'
-app.config['MONGO_URL'] = 'mongodb://localhost:27020/mydb'
+app.config['MONGO_URL'] = 'mongodb://localhost:27017/mongoExample'
 
 mongo = PyMongo(app)
+
+def checkArguments(check_list,req):
+	for argument in check_list:
+		if not argument in req:
+			return False
+	return True
+
+def checkToken(token,userDb):
+
+	secret_key = config.secretKey
+	try:
+		jwt_decode = jwt.decode(token,secret_key,algorithms=['HS256'])
+		if jwt_decode['user_id'] == userDb['_id']:
+			return True
+		return False
+
+	except jwt.InvalidTokenError:
+		return False	
 
 @app.route('/')
 def index():
@@ -19,33 +42,55 @@ def index():
 @app.route('/login',methods=['POST'])
 def login():
 	users = mongo.db.users
-	login_user = users.find_one({"name":request.form['username']})
+	req = request.get_json()
+	secret_key = config.secretKey
+	check_list = ['token','username','password']
+	if checkArguments(check_list,req) :
+		token = req['token']
+		username = req['username']
+		password = req['password']
+		userDb = users.find_one({'username':username})
+		if userDb:
+			pwdHash = userDb['password']
 
-	if login_user:
-		if bcrypt.hashpw(request.form['pass'].encode('UTF-8'),login_user['password'].encode('UTF-8')) == login_user['password'].encode('UTF-8'):
-			session['username'] = request.form['username']
-			return redirect(url_for('index'))
-
-		return 'Invalid username/password'
-
-	return 'No User found'
-
+			if checkToken(token,userDb):
+				if bcrypt.hashpw(password.encode('UTF-8'),pwdHash.encode('UTF-8')) == pwdHash.encode('UTF-8'):
+					response = {'status':True,'msg':'successfully login'}
+					return jsonify(response)
+				return 'wrong password'
+			return 'Wrong Token'
+		return 'No user'
+	return 'Username/password/token not provided'
 
 @app.route('/register',methods=['POST','GET'])
 def register():
-	if request.method == 'POST':
-		users = mongo.db.users
-		existing_user = users.find_one({'name':request.form['username']})
+	
+	users = mongo.db.users
+	check_list = ['username','password']
+	req = request.get_json()
+
+	if checkArguments(check_list,req):
+		username = req['username']
+		password = req['password']
+		existing_user = users.find_one({'username':username})
 
 		if existing_user is None:
-			hashpwd = bcrypt.hashpw(request.form['pass'].encode('UTF-8'),bcrypt.gensalt())
-			users.insert({'name':request.form['username'],'password':hashpwd})
-			session['username'] = request.form['username']
-			return url_for('index')
+			hashpwd = bcrypt.hashpw(password.encode('UTF-8'),bcrypt.gensalt())
+			user_id = repr(ObjectId())
+			user = {'_id':user_id,'username':username,'password':hashpwd}
+			user_json = json.loads(json_util.dumps(user))
+			secret_key = config.secretKey
+			payload = {'user_id':user_json['_id'],'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=60),
+                'iat': datetime.datetime.utcnow(),}
+			token = jwt.encode(payload,secret_key,algorithm='HS256')
+			user_json['token'] = token
+			users.insert(user_json)
+			response_json ={'id':user_json['_id'],'token':user_json['token']}
+			return jsonify(response_json)
 
-		return 'username already exist'
+		return 'user exist'
 
-	return render_template('register.html')
+	return ' provide suitable arguments'
 
 if __name__ == '__main__':
 	app.secret_key = 'mysecret'
